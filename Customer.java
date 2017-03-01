f2b870ca48ce62e581643fef00cd7d0f0fb3872f
tobi5519 <tobi5519@stud.kea.dk>
2017-03-01 20:35:33 +0100

    ...

--- Customer.java
+++ Customer.java
@@ -8,13 +8,14 @@ public class Customer{
     public void setWealth(double newWealth){
         this.wealth = newWealth;
     }
+//    WHY ALWAYS CREATE SHIT WE DON'T NEED?!!
     public String getName(){
         return this.name;
     }
     public double getWealth(){
         return this.wealth;
     }
-
+//    Subtracts 'price' from 'wealth' if 'price' is less than 'wealth'
     public void orderGarbageMan(double orderAmount){
         if (orderAmount < this.wealth){
             this.wealth -= orderAmount;
--- GarbageManUnion.java
+++ GarbageManUnion.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 public class GarbageManUnion{
+//    Creating an ArrayList of objects from the GarbageMan class
     private ArrayList<GarbageMan> GMAL = new ArrayList();
     private int xCount;
      public void addGarbageman(GarbageMan newGarbageMan){
@@ -15,8 +16,8 @@ public class GarbageManUnion{
             System.out.println(GMAL.get(i - 1).getName() + " has a price of kr. " + GMAL.get(i - 1).getPrice());
         }
     }
+//    Iterating over the size value(length) of the ArrayList and printing all objects with a 'price' over 100
     public int howManyExpensiveGarbageMen(){
-
         for (int i = GMAL.size(); i > 0; i--){
             if (GMAL.get(i - 1).getPrice() > 100){
                 System.out.println(GMAL.get(i - 1).getName() + " is an ass, he costs " + GMAL.get(i - 1).getPrice());
